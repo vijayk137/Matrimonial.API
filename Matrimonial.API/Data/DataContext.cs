dfcd447104d93f4fab883f15d627ce1ebb09b6cd
vijayk137 <v.vijay137@gmail.com>
2020-07-18 03:46:48 +0530

    initial commit

---
+++ Matrimonial.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Matrimonial.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Matrimonial.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {
+
+        }
+        public DbSet<Value> Values { get; set; }
+        
+    }
+}
